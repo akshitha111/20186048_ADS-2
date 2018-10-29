2ece4b69c10a0b7913c11d387c7aeaae8f22b03d
akshitha111 <akshitha2408@msitprogram.net>
2018-10-29 18:56:22 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m1/1.1 Graph/Solution.java
+++ ADS-2-assignments/m1/1.1 Graph/Solution.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 interface Graph  {
     public int V();
     public int E();
@@ -8,8 +9,72 @@ interface Graph  {
     public boolean hasEdge(int v, int w);
 }
 
+class AdjacentList {
+    private static final String NEWLINE = System.getProperty("line.separator");
 
-class Solution {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public AdjacentList(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        E++;
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    public Iterable<Integer> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (int w : adj[v]) {
+                s.append(w + " ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+}
+
+
+
+
+
+
+
+
+
+public final class Solution {
 	private Solution() {
 
 	}public static void main(String[] args) {
@@ -18,7 +83,7 @@ class Solution {
 		int vertices = Integer.parseInt(sc.nextLine());
 		int edges = Integer.parseInt(sc.nextLine());
 		String[] tokens = sc.nextLine().split(",");
-		System.out.println(Arrays.toString(tokens));
+		//System.out.println(Arrays.toString(tokens));
 		/*switch (input) {
 			case "List":
 				AdjacentList al = new AdjacentList();
@@ -32,7 +97,7 @@ class Solution {
 		}*/
 		for(int i = 0; i < vertices; i++) {
 					String[] check = sc.nextLine().split(" ");
-					System.out.println(Arrays.toString(check));
+					//System.out.println(Arrays.toString(check));
 				}
 				
 		
