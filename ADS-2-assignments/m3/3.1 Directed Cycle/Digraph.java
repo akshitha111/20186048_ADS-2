e8d6307408a126cc1f31ee4aa43c1113e73452dd
akshitha111 <akshitha2408@msitprogram.net>
2018-10-31 16:15:40 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m3/3.1 Directed Cycle/Digraph.java
+++ ADS-2-assignments/m3/3.1 Directed Cycle/Digraph.java
@@ -1,91 +1,53 @@
-import java.util.NoSuchElementException;
+/**
+ * Class for digraph.
+ */
 public class Digraph {
+    /**
+     * { newline variable }.
+     */
     private static final String NEWLINE = System.getProperty("line.separator");
-
-    private final int V;           // number of vertices in this digraph
-    private int E;                 // number of edges in this digraph
+    /**
+     * { vertices variable }.
+     */
+    private final int vertices;           // number of vertices in this digraph
+    /**
+     * { edges variable }.
+     */
+    private int edges;                 // number of edges in this digraph
+    /**
+     * { bag variable }.
+     */
     private Bag<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+    /**
+     * { indegree variable }.
+     */
     private int[] indegree;        // indegree[v] = indegree of vertex v
-    
     /**
      * Initializes an empty digraph with <em>V</em> vertices.
      *
-     * @param  V the number of vertices
+     * @param  vertexOne the number of vertices
      * @throws IllegalArgumentException if {@code V < 0}
      */
-    public Digraph(int V) {
-        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
-        this.V = V;
-        this.E = 0;
-        indegree = new int[V];
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
+    public Digraph(final int vertexOne) {
+        if (vertexOne < 0) {
+         throw new IllegalArgumentException(
+            "Number of vertices in a Digraph must be nonnegative");
+     }
+        this.vertices = vertexOne;
+        this.edges = 0;
+        indegree = new int[vertexOne];
+        adj = (Bag<Integer>[]) new Bag[vertexOne];
+        for (int v = 0; v < vertexOne; v++) {
             adj[v] = new Bag<Integer>();
         }
     }
-
-    /**  
-     * Initializes a digraph from the specified input stream.
-     * The format is the number of vertices <em>V</em>,
-     * followed by the number of edges <em>E</em>,
-     * followed by <em>E</em> pairs of vertices, with each entry separated by whitespace.
-     *
-     * @param  in the input stream
-     * @throws IllegalArgumentException if the endpoints of any edge are not in prescribed range
-     * @throws IllegalArgumentException if the number of vertices or edges is negative
-     * @throws IllegalArgumentException if the input stream is in the wrong format
-     */
-    /*public Digraph(In in) {
-        try {
-            this.V = in.readInt();
-            if (V < 0) throw new IllegalArgumentException("number of vertices in a Digraph must be nonnegative");
-            indegree = new int[V];
-            adj = (Bag<Integer>[]) new Bag[V];
-            for (int v = 0; v < V; v++) {
-                adj[v] = new Bag<Integer>();
-            }
-            int E = in.readInt();
-            if (E < 0) throw new IllegalArgumentException("number of edges in a Digraph must be nonnegative");
-            for (int i = 0; i < E; i++) {
-                int v = in.readInt();
-                int w = in.readInt();
-                addEdge(v, w); 
-            }
-        }
-        catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("invalid input format in Digraph constructor", e);
-        }
-    }*/
-
-    /**
-     * Initializes a new digraph that is a deep copy of the specified digraph.
-     *
-     * @param  G the digraph to copy
-     */
-    public Digraph(Digraph G) {
-        this(G.V());
-        this.E = G.E();
-        for (int v = 0; v < V; v++)
-            this.indegree[v] = G.indegree(v);
-        for (int v = 0; v < G.V(); v++) {
-            // reverse so that adjacency list is in same order as original
-            Stack<Integer> reverse = new Stack<Integer>();
-            for (int w : G.adj[v]) {
-                reverse.push(w);
-            }
-            for (int w : reverse) {
-                adj[v].add(w);
-            }
-        }
-    }
-        
     /**
      * Returns the number of vertices in this digraph.
      *
      * @return the number of vertices in this digraph
      */
-    public int V() {
-        return V;
+    public int vertex() {
+        return vertices;
     }
 
     /**
@@ -93,15 +55,19 @@ public class Digraph {
      *
      * @return the number of edges in this digraph
      */
-    public int E() {
-        return E;
+    public int edge() {
+        return edges;
     }
-
-
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    /**
+     * validate vertex function.
+     *
+     * @param      v     { parameter_description }
+     */
+    private void validateVertex(final int v) {
+        if (v < 0 || v >= vertices) {
+            throw new IllegalArgumentException("vertex "
+             + v + " is not between 0 and " + (vertices - 1));
+        }
     }
 
     /**
@@ -109,37 +75,40 @@ public class Digraph {
      *
      * @param  v the tail vertex
      * @param  w the head vertex
-     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     * @throws IllegalArgumentException
+     *  unless both {@code 0 <= v < V} and {@code 0 <= w < V}
      */
-    public void addEdge(int v, int w) {
+    public void addEdge(final int v, final int w) {
         validateVertex(v);
         validateVertex(w);
         adj[v].add(w);
         indegree[w]++;
-        E++;
+        edges++;
     }
 
     /**
      * Returns the vertices adjacent from vertex {@code v} in this digraph.
      *
      * @param  v the vertex
-     * @return the vertices adjacent from vertex {@code v} in this digraph, as an iterable
+     * @return the vertices adjacent from vertex {@code v}
+     *  in this digraph, as an iterable
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public Iterable<Integer> adj(int v) {
+    public Iterable<Integer> adj(final int v) {
         validateVertex(v);
         return adj[v];
     }
 
     /**
-     * Returns the number of directed edges incident from vertex {@code v}.
+     * Returns the number of directed edges incident
+     *  from vertex {@code v}.
      * This is known as the <em>outdegree</em> of vertex {@code v}.
      *
      * @param  v the vertex
-     * @return the outdegree of vertex {@code v}               
+     * @return the outdegree of vertex {@code v}
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public int outdegree(int v) {
+    public int outdegree(final int v) {
         validateVertex(v);
         return adj[v].size();
     }
@@ -149,10 +118,10 @@ public class Digraph {
      * This is known as the <em>indegree</em> of vertex {@code v}.
      *
      * @param  v the vertex
-     * @return the indegree of vertex {@code v}               
+     * @return the indegree of vertex {@code v}
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-    public int indegree(int v) {
+    public int indegree(final int v) {
         validateVertex(v);
         return indegree[v];
     }
@@ -163,8 +132,8 @@ public class Digraph {
      * @return the reverse of the digraph
      */
     public Digraph reverse() {
-        Digraph reverse = new Digraph(V);
-        for (int v = 0; v < V; v++) {
+        Digraph reverse = new Digraph(vertices);
+        for (int v = 0; v < vertices; v++) {
             for (int w : adj(v)) {
                 reverse.addEdge(w, v);
             }
@@ -175,13 +144,14 @@ public class Digraph {
     /**
      * Returns a string representation of the graph.
      *
-     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,  
+     * @return the number of vertices <em>V</em>,
+     * followed by the number of edges <em>E</em>,
      *         followed by the <em>V</em> adjacency lists
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
+        s.append(vertices + " vertices, " + edges + " edges " + NEWLINE);
+        for (int v = 0; v < vertices; v++) {
             s.append(String.format("%d: ", v));
             for (int w : adj[v]) {
                 s.append(String.format("%d ", w));
@@ -191,3 +161,6 @@ public class Digraph {
         return s.toString();
     }
 }
+
+
+
--- ADS-2-assignments/m3/3.1 Directed Cycle/DirectedCycle.java
+++ ADS-2-assignments/m3/3.1 Directed Cycle/DirectedCycle.java
@@ -1,81 +1,112 @@
+/**
+ * Class for directed cycle.
+ */
 public class DirectedCycle {
-    private boolean[] marked;        // marked[v] = has vertex v been marked?
-    private int[] edgeTo;            // edgeTo[v] = previous vertex on path to v
-    private boolean[] onStack;       // onStack[v] = is vertex on the stack?
-    private Stack<Integer> cycle;    // directed cycle (or null if no such cycle)
+    /**
+     * { marked variable }.
+     */
+    private boolean[] marked;
+    // marked[v] = has vertex v been marked?
+    /**
+     * { edgeTo variable }.
+     */
+    private int[] edgeTo;
+    // edgeTo[v] = previous vertex on path to v
+    /**
+     * { onstack variable }.
+     */
+    private boolean[] onStack;
+    // onStack[v] = is vertex on the stack?
+    /**
+     * { cycle variable }.
+     */
+    private Stack<Integer> cycle;
+    // directed cycle (or null if no such cycle)
 
     /**
      * Determines whether the digraph {@code G} has a directed cycle and, if so,
      * finds such a cycle.
-     * @param G the digraph
+     * @param graph the digraph
      */
-    public DirectedCycle(Digraph G) {
-        marked  = new boolean[G.V()];
-        onStack = new boolean[G.V()];
-        edgeTo  = new int[G.V()];
-        for (int v = 0; v < G.V(); v++)
-            if (!marked[v] && cycle == null) dfs(G, v);
+    public DirectedCycle(final Digraph graph) {
+        marked  = new boolean[graph.vertex()];
+        onStack = new boolean[graph.vertex()];
+        edgeTo  = new int[graph.vertex()];
+        for (int v = 0; v < graph.vertex(); v++) {
+            if (!marked[v] && cycle == null) {
+                dfs(graph, v);
+            }
+        }
     }
 
-    // check that algorithm computes either the topological order or finds a directed cycle
-    private void dfs(Digraph G, int v) {
-        onStack[v] = true;
-        marked[v] = true;
-        for (int w : G.adj(v)) {
+    /**
+     * { dfs function }.
+     *
+     * @param      gra    { parameter_description }
+     * @param      ver     { parameter_description }
+     */
+    private void dfs(final Digraph gra, final int ver) {
+        onStack[ver] = true;
+        marked[ver] = true;
+        for (int w : gra.adj(ver)) {
 
             // short circuit if directed cycle found
-            if (cycle != null) return;
-
-            // found new vertex, so recur
-            else if (!marked[w]) {
-                edgeTo[w] = v;
-                dfs(G, w);
-            }
-
-            // trace back directed cycle
-            else if (onStack[w]) {
+            if (cycle != null) {
+                return;
+            } else if (!marked[w]) {
+                edgeTo[w] = ver;
+                dfs(gra, w);
+            } else if (onStack[w]) {
                 cycle = new Stack<Integer>();
-                for (int x = v; x != w; x = edgeTo[x]) {
+                for (int x = ver; x != w; x = edgeTo[x]) {
                     cycle.push(x);
                 }
                 cycle.push(w);
-                cycle.push(v);
+                cycle.push(ver);
                 assert check();
             }
         }
-        onStack[v] = false;
+        onStack[ver] = false;
     }
 
     /**
      * Does the digraph have a directed cycle?
-     * @return {@code true} if the digraph has a directed cycle, {@code false} otherwise
+     * @return {@code true} if the digraph has
+     *  a directed cycle, {@code false} otherwise
      */
     public boolean hasCycle() {
         return cycle != null;
     }
 
     /**
-     * Returns a directed cycle if the digraph has a directed cycle, and {@code null} otherwise.
-     * @return a directed cycle (as an iterable) if the digraph has a directed cycle,
+     * Returns a directed cycle if the digraph has
+     * a directed cycle, and {@code null} otherwise.
+     * @return a directed cycle (as an iterable) if
+     * the digraph has a directed cycle,
      *    and {@code null} otherwise
      */
     public Iterable<Integer> cycle() {
         return cycle;
     }
-
-
-    // certify that digraph has a directed cycle if it reports one
+    /**
+     * { check function }.
+     *
+     * @return     { description_of_the_return_value }
+     */
     private boolean check() {
 
         if (hasCycle()) {
             // verify cycle
             int first = -1, last = -1;
             for (int v : cycle()) {
-                if (first == -1) first = v;
+                if (first == -1) {
+                    first = v;
+                }
                 last = v;
             }
             if (first != last) {
-                System.err.printf("cycle begins with %d and ends with %d\n", first, last);
+                System.err.printf(
+                "cycle begins with %d and ends with %d\n", first, last);
                 return false;
             }
         }
@@ -84,3 +115,5 @@ public class DirectedCycle {
         return true;
     }
 }
+
+
