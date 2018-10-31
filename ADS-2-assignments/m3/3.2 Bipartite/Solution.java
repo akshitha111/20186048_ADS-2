333f856a1892cf5395e81debbd311fa3ed535163
akshitha111 <akshitha2408@msitprogram.net>
2018-10-31 16:59:29 +0530

    "ads2_m3_a2_1: 3/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m3/3.2 Bipartite/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Solution {
+	private Solution() {
+
+	}public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int vertices = Integer.parseInt(sc.nextLine());
+		int edges = Integer.parseInt(sc.nextLine());
+		Digraph dg = new Digraph(vertices);
+		for (int i = 0; i < edges; i++) {
+			String[] tokens = sc.nextLine().split(" ");
+			dg.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+		}
+		DirectedCycle dc = new DirectedCycle(dg);
+        if (dc.hasCycle()) {
+            System.out.println("Graph is bipartite");
+        } else {
+            System.out.println("Graph is not a bipartite");
+        }
+	}
+}
