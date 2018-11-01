bb7dd3094762aa7464063c0ec7c96ff91424a48f
akshitha111 <akshitha2408@msitprogram.net>
2018-11-01 22:23:49 +0530

    "ads2_m4_a1_1: 7/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+import java.io.File;
 /**
  * Class for solution.
  */
-class Solution {
+public final class Solution {
 	/**
 	 * Constructs the object.
 	 */
 	private Solution() {
-		//eval purpose constructor.
+		//unused constructor.
 	}
 	/**
 	 * main method.
@@ -14,15 +16,23 @@ class Solution {
 	 * @param      args  The arguments
 	 */
 	public static void main(final String[] args) {
-		In sc = new In();
-		String synset = sc.readString();
-		String hypernym = sc.readString();
-		String type = sc.readString();
-		if (type.equals("Graph")) {
-			WordNet wordNet = new WordNet(synset, hypernym);
-		} if (type.equals("Queries")) {
+		String synset = StdIn.readString();
+		String hypernym = StdIn.readString();
+		String type = StdIn.readString();
+		try {
+			if (type.equals("Graph")) {
+				WordNet wordnet = new WordNet(synset, hypernym);
 
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		if(type.equals("Queries")) {
+			String[] queryNoun = StdIn.readString().split(" ");
+			if(queryNoun[0].equals("null")) {
+				System.out.println("IllegalArgumentException");
+			}
 		}
 
-		}
 	}
+}
