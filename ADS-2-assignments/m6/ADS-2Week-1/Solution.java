7427869590dc5f0bc58d2135f198c68e68d36f0b
akshitha111 <akshitha2408@msitprogram.net>
2018-11-03 12:57:03 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m6/ADS-2Week-1/Solution.java
+++ ADS-2-assignments/m6/ADS-2Week-1/Solution.java
@@ -5,8 +5,22 @@ class PageRank {
 	PageRank(Digraph digraph) {
 		this.dg = digraph;
 	}
-
-	double getPR(int v) {
+	public double getPR(int v) {
+		return dg.indegree(v);
+	}
+	public void computePR(int v) {
+		 double compute = getPR(v) / dg.outdegree(v);
+		 System.out.println(v + ":" + compute);
+		 //compute;
+	}
+}
+	/*private Digraph dg;
+	private double pr;
+	PageRank(Digraph digraph) {
+		this.dg = digraph;
+	}
+*/
+	/*double getPR(int v) {
 		pr = 0;
 		for (int i = 0; i < 1000; i++) {
 		for (Integer j : dg.adj(v)){
@@ -15,11 +29,11 @@ class PageRank {
 	}
 
 		return pr;
-	}
-	public String toString() {
+	}*/
+	/*public String toString() {
         StringBuilder s = new StringBuilder();
         //s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < dg.V(); v++) {
+        for (int v = 0; v < digraph.V(); v++) {
             s.append(String.format("%d: - ", v));
             //for (int w : adj[v]) {
               //  s.append(String.format("%d ", w));
@@ -28,8 +42,8 @@ class PageRank {
             s.append("\n");
         }
         return s.toString();
-    }
-}
+    }*/
+
 
 class WebSearch {
 
@@ -48,19 +62,31 @@ public class Solution {
 		//	StdIn.readLine();
 		//}	
 		Digraph dg = new Digraph(vertices);
+		PageRank pr = new PageRank(dg);
+
+		int count = 0;
 		for (int i = 0; i < vertices; i++) {
 			String[] tokens = sc.nextLine().split(" ");
 			for (int j = 1; j < tokens.length; j++) {
+				
 				dg.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[j]));
-			}
+			}count = i;
 		}
 		System.out.println(dg);
+		pr.computePR(count);
 		System.out.println();
 		// Create page rank object and pass the graph object to the constructor
-		PageRank pr = new PageRank(dg);
+		
+		/*for (int i = 0; i < vertices; i++) {
+			String[] tokens = sc.nextLine().split(" ");
+			for (int j = 1; j < tokens.length; j++) {
+				pr.computePR(j);
+			}
+		}*/
 		
 		// print the page rank object
-		//System.out.println(pr);
+		//pr.calc(vertices);
+		//pr.computePR(vertices);
 		// This part is only for the final test case
 		
 		// File path to the web content
