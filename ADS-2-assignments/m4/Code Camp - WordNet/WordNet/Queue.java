6603ad42b08d31f2623c789a20ca23e5429caa98
akshitha111 <akshitha2408@msitprogram.net>
2018-11-04 16:36:57 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Queue.java
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Queue.java
@@ -1,64 +1,64 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 /**
- * List of Queue.
+ * List of .
  *
  * @param      <Item>  The item
  */
-public class Queue<Item> {
+public class Queue<Item> implements Iterable<Item> {
     /**
-     * {number of elements on queue}.
+     * Integer variable.
      */
     private int n;
     /**
-     * {beginning of queue}.
+     * Node first.
      */
     private Node first;
     /**
-     * {end of queue}.
+     * Node last.
      */
     private Node last;
-
     /**
      * Class for node.
      */
     private class Node {
         /**
-         * {Item}.
+         * Item variable.
          */
         private Item item;
         /**
-         * {Node of type next}.
+         * Node variable.
          */
         private Node next;
     }
-
     /**
      * Create an empty queue.
      */
-    Queue() {
+    public Queue() {
         first = null;
         last  = null;
     }
-
     /**
      * Is the queue empty?
-     * @return     {Boolean}
+     *
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return first == null;
     }
-
     /**
      * Return the number of items in the queue.
-     * @return     {Integer}
+     *
+     * @return     number of elements.
      */
-    public int size() {
+    public  int size() {
         return n;
     }
-
     /**
-     * Return the item least recently added to the queue.
-     * Throw an exception if the queue is empty.
-     * @return     {Item}
+     * Return the item least recently added to the queue. Throw an exception if
+     * the queue is empty.
+     *
+     * @return     first item.
      */
     public Item peek() {
         if (isEmpty()) {
@@ -66,10 +66,8 @@ public class Queue<Item> {
         }
         return first.item;
     }
-
-
     /**
-     * {Add the item to the queue}.
+     * Add the item to the queue.
      *
      * @param      item  The item
      */
@@ -87,23 +85,82 @@ public class Queue<Item> {
     }
 
     /**
-     * Remove and return the item on the queue least recently added.
-     * Throw an exception if the queue is empty.
-     * @return     {Item}
+     * Remove and return the item on the queue least recently added. Throw an
+     * exception if the queue is empty.
+     *
+     * @return     item.
      */
     public Item dequeue() {
         if (isEmpty()) {
-            throw new RuntimeException(
-                "Queue underflow");
+            throw new RuntimeException("Queue underflow");
         }
         Item item = first.item;
         first = first.next;
         n--;
-        // to avoid loitering
         if (isEmpty()) {
             last = null;
         }
         return item;
     }
+
+    /**
+     * Return string representation.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item + " ");
+        }
+        return s.toString();
+    }
+    /**
+     * Return an iterator that iterates over the items on the queue in FIFO
+     * order.
+     *
+     * @return     list iterator.
+     */
+    public Iterator<Item> iterator()  {
+        return new ListIterator();
+    }
+    /**
+     * Class for list iterator.
+     */
+    private class ListIterator implements Iterator<Item> {
+        /**
+         * Node variables.
+         */
+        private Node current = first;
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            return current != null;
+        }
+        /**
+         * remove.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * Item.
+         *
+         * @return     item.
+         */
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
 }
+
+
 
