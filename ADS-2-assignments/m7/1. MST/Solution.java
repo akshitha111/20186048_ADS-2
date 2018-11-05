e2887f434861a5abc229fa2e6c8088f785cdb1c3
akshitha111 <akshitha2408@msitprogram.net>
2018-11-05 15:57:59 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m7/1. MST/Solution.java
+++ ADS-2-assignments/m7/1. MST/Solution.java
@@ -3,30 +3,35 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int vertices = Integer.parseInt(sc.nextLine());
-		int edges = Integer.parseInt(sc.nextLine());
-		EdgeWeightedGraph edgeWeightedGraph = new EdgeWeightedGraph(vertices);
-		
-		for (int i = 0; i < edges; i++) {
-			String[] tokens = sc.nextLine().split(" ");
-			Edge edge = new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2]));
-			edgeWeightedGraph.addEdge(edge);
-		}
-		KruskalMST kruskal = new KruskalMST(edgeWeightedGraph);
-		System.out.printf("%.5f\n",  kruskal.weight());
+    }
+    /**
+     * { Client program }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vertices = Integer.parseInt(sc.nextLine());
+        int edges = Integer.parseInt(sc.nextLine());
+        EdgeWeightedGraph edgeWeightedGraph =
+        new EdgeWeightedGraph(vertices);
+        for (int i = 0; i < edges; i++) {
+            String[] tokens = sc.nextLine().split(" ");
+            Edge edge = new Edge(Integer.parseInt(tokens[0]),
+                Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2]));
+            edgeWeightedGraph.addEdge(edge);
+        }
+        KruskalMST kruskal = new KruskalMST(edgeWeightedGraph);
+        System.out.printf("%.5f\n",  kruskal.weight());
 
-	}
+    }
 }
+
+
+
+
