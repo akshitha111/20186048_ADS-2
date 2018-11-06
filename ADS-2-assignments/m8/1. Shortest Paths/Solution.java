8f16d866138590ed2246efc5b48245cce080735c
akshitha111 <akshitha2408@msitprogram.net>
2018-11-06 21:51:10 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m8/1. Shortest Paths/Solution.java
+++ ADS-2-assignments/m8/1. Shortest Paths/Solution.java
@@ -45,6 +45,7 @@ public final class Solution {
                 System.out.println((int)
                     dijkstra.distTo(hash.get(check1[1])));
             }
+            queries--;
         }
     }
 }
