1780c48ebfdcfb38011e3f9dd15ba5ff4972e189
akshitha111 <akshitha2408@msitprogram.net>
2018-11-10 12:08:52 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Stack.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Stack.java
@@ -1,3 +1,25 @@
+/******************************************************************************
+ *  Compilation:  javac Stack.java
+ *  Execution:    java Stack < input.txt
+ *  Dependencies: StdIn.java StdOut.java
+ *  Data files:   https://algs4.cs.princeton.edu/13stacks/tobe.txt
+ *
+ *  A generic stack, implemented using a singly linked list.
+ *  Each stack element is of type Item.
+ *
+ *  This version uses a static nested class Node (to save 8 bytes per
+ *  Node), whereas the version in the textbook uses a non-static nested
+ *  class (for simplicity).
+ *  
+ *  % more tobe.txt 
+ *  to be or not to - be - - that - - - is
+ *
+ *  % java Stack < tobe.txt
+ *  to be not that or be (2 left on stack)
+ *
+ ******************************************************************************/
+
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -146,5 +168,4 @@ public class Stack<Item> implements Iterable<Item> {
             return item;
         }
     }
-
 }
