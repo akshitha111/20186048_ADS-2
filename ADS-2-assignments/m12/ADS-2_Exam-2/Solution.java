c7889d0736a5bdf83cca6e87859ae43d7b7834a9
akshitha111 <akshitha2408@msitprogram.net>
2018-11-10 12:07:15 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -2,17 +2,23 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
-		Scanner sc = new Scanner(System.in);
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused constructor.
+    }
+    /**
+     * { Client Program }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // Self loops are not allowed...
+        // Parallel Edges are allowed...
+        // Take the Graph input here...
+        Scanner sc = new Scanner(System.in);
         int vertices = Integer.parseInt(sc.nextLine());
         int edges = Integer.parseInt(sc.nextLine());
         EdgeWeightedDigraph digraph = new EdgeWeightedDigraph(vertices);
@@ -21,49 +27,63 @@ public class Solution {
                 int verOne = Integer.parseInt(tokens[0]);
                 int edgeOne = Integer.parseInt(tokens[1]);
                 Double weight = Double.parseDouble(tokens[2]);
-                Edge edge = new Edge(verOne,edgeOne,weight);
+                Edge edge = new Edge(verOne, edgeOne, weight);
                 digraph.addEdge(edge);
-                
-        }        
+        }
         String caseToGo =  sc.nextLine();
-		switch (caseToGo) {
-		case "Graph":
-		System.out.println(digraph);
-			//Print the Graph Object.		 
-		break;
-		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			// 
-			String[] check = sc.nextLine().split(" ");
-			int source = Integer.parseInt(check[0]);
-			int destination = Integer.parseInt(check[1]);
-			DijkstraUndirectedSP dijik = new DijkstraUndirectedSP(digraph, source);
-			if(!dijik.hasPathTo(destination)) {
-				System.out.println("No Path Found.");
-			}
-			else {
-				System.out.println(dijik.distTo[destination]);
-			}
-			break;		
-		case "ViaPaths":
-			String[] values = sc.nextLine().split(" ");
-			int sourceOne = Integer.parseInt(values[0]);
-			int via = Integer.parseInt(values[1]);
-			int destinationOne = Integer.parseInt(values[2]);
+        switch (caseToGo) {
+        case "Graph":
+        System.out.println(digraph);
+            //Print the Graph Object.
+            break;
+        case "DirectedPaths":
+            // Handle the case of DirectedPaths, where two integers are given.
+            // First is the source and second is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            //
+            String[] check = sc.nextLine().split(" ");
+            int source = Integer.parseInt(check[0]);
+            int destination = Integer.parseInt(check[1]);
+            DijkstraUndirectedSP dijik = new
+            DijkstraUndirectedSP(digraph, source);
+            if (!dijik.hasPathTo(destination)) {
+                System.out.println("No Path Found.");
+            } else {
+                System.out.println(dijik.distTo[destination]);
+            }
+            break;
+        case "ViaPaths":
+            String[] values = sc.nextLine().split(" ");
+            int sourceOne = Integer.parseInt(values[0]);
+            int via = Integer.parseInt(values[1]);
+            int destinationOne = Integer.parseInt(values[2]);
 
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			break;
+            // Handle the case of ViaPaths, where three integers are given.
+            // First is the source and second is
+            // the via is the one where path should pass throuh.
+            // third is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            break;
 
-		default:
-			break;
-		}
+        default:
+            break;
+        }
 
-	}
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
