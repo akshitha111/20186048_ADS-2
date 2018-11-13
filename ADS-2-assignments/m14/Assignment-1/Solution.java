eba1f6170cdba7229c3550bb9473d290fe9011dc
akshitha111 <akshitha2408@msitprogram.net>
2018-11-13 16:16:15 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m14/Assignment-1/Solution.java
+++ ADS-2-assignments/m14/Assignment-1/Solution.java
@@ -3,40 +3,51 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused constructor.
-	}
-	/**
-	 * { Client program }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		String[] words = loadWords();
-		//Your code goes here...
-		TST<Integer> tst = new TST<Integer>();
-		Scanner sc = new Scanner(System.in);
-		String prefix = sc.nextLine();
-		int j = 0;
-		for (String word : words) {
-			SuffixArray sa = new SuffixArray(word);
-			for (int i = 0; i < word.length(); i++) {
-				tst.put(sa.select(i), j++);
-			}
-		}for (String word : tst.keysWithPrefix(prefix)) {
-			System.out.println(word);
-		}
-	}
-
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused constructor.
+    }
+    /**
+     * { Client program }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String[] words = loadWords();
+        //Your code goes here...
+        TST<Integer> tst = new TST<Integer>();
+        Scanner sc = new Scanner(System.in);
+        String prefix = sc.nextLine();
+        int j = 0;
+        for (String word : words) {
+            SuffixArray sa = new SuffixArray(word);
+            for (int i = 0; i < word.length(); i++) {
+                tst.put(sa.select(i), j++);
+            }
+        }
+        for (String word : tst.keysWithPrefix(prefix)) {
+            System.out.println(word);
+        }
+    }
+    /**
+     * Loads words.
+     *
+     * @return     { words }.
+     */
+    public static String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
+
+
+
+
+
+
 
 
 
