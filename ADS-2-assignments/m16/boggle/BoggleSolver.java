25521ad3c3edbb3a80249805e1c6b8978bf9112b
akshitha111 <akshitha2408@msitprogram.net>
2018-11-16 09:59:08 +0530

    "ads2_m16_a1_1: 29/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m16/boggle/BoggleSolver.java
+++ ADS-2-assignments/m16/boggle/BoggleSolver.java
@@ -1,80 +1,91 @@
 import java.util.Set;
 import java.util.TreeSet;
 public class BoggleSolver {
-	private TrieST<Integer> dictionaryTrie;
-	private static final char Q_LETTER = 'Q';
-    private static final String QU_STRING = "QU";
+	// Initializes the data structure using the given
+	// array of strings as the dictionary.
+	// (You can assume each word in the dictionary
+	//  contains only the uppercase letters A through Z.)
 
-	// Initializes the data structure using the given array of strings as the dictionary.
-	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
-	public BoggleSolver(String[] dictionary) {
-		int[] points = {0, 0, 0, 1, 1, 2, 3, 5, 11};
+	private TrieST<Integer> dictionaryTrie;
+	private Set<String> validWords;
+	private boolean[][] marked;
+	public BoggleSolver(final String[] dictionary) {
 		dictionaryTrie = new TrieST<Integer>();
+		validWords = new TreeSet<String>();
+		int[] points = {0, 0, 0, 1, 1, 2, 3, 5, 11};
 		for (String word : dictionary) {
-			dictionaryTrie.put(word, points[word.length()]);
-				if (word.length() >= 8) {
-					dictionaryTrie.put(word, 11);
-				}
-				else {
-					dictionaryTrie.put(word, points[word.length()]);
-				}
+			if (word.length() >= 8) {
+				dictionaryTrie.put(word, 11);
+			} else {
+				dictionaryTrie.put(word, points[word.length()]);
+			}
 		}
 	}
-
 	// Returns the set of all valid words in the given Boggle board, as an Iterable.
-	public Iterable<String> getAllValidWords(BoggleBoard board) {
-		if (board == null) 
-         throw new java.lang.IllegalArgumentException("Board is null!");
-      Set<String> foundWords = new TreeSet<String>();
-      for (int row = 0; row < board.rows(); row++) {
-         for (int col = 0; col < board.cols(); col++) {
-            String charSequence = addLetter("", board.getLetter(row, col));
-            boolean marked[][] = new boolean[board.rows()][board.cols()];
-            marked[row][col] = true;
-            dfs(foundWords, charSequence, marked, row, col, board);
-         }
-      }
-      return foundWords;
-   }
+	public Iterable<String> getAllValidWords(final BoggleBoard board) {
+		if (board == null) {
+			throw new IllegalArgumentException("board is null");
+		}
+		marked = new boolean[board.rows()][board.cols()];
+		for (int i = 0; i < board.rows(); i++) {
+			for (int j = 0; j < board.cols(); j++) {
+				String st = appendCharacter("", board.getLetter(i, j));
+				dfs(board, marked, i, j, st);
+			}
+		}
+		return validWords;
+	}
+	private String appendCharacter(String st, final char c) {
+		if (c == 'Q') {
+			st += "QU";
+			return st;
+		} else {
+			st += c;
+			return st;
+		}
+	}
+	private boolean isValidWord(final String word) {
+		if (word.length() < 3) {
+			return false;
+		}
+		return dictionaryTrie.contains(word);
+	}
 
-   private void dfs(Set<String> foundWords, String charSequence, boolean[][] marked,
-         int startRow, int startCol, BoggleBoard board) {
-      if (isValidWord(charSequence) ) foundWords.add(charSequence);
-      for (int row = Math.max(0, startRow - 1); row <= Math.min(board.rows() - 1, startRow + 1); row++) {
-         for (int col = Math.max(0, startCol - 1); col <= Math.min(board.cols() - 1,startCol + 1); col++) {
-            if (marked[row][col]) {
-            	continue;
-            }
-            // if (!dictionarytrie.keysWithPrefix(charSequence)) {
-            // 	continue;
-            // }
-            marked[row][col] = true;
-            dfs(foundWords, addLetter(charSequence, board.getLetter(row, col)), marked, row, col, board);
-            marked[row][col] = false;
-         }
-      }
-   }
+	public void dfs(final BoggleBoard board, final boolean[][] marked,
+	                final int rows, final int cols, final String word) {
+		if (!dictionaryTrie.hasPrefix(word)) {
+			return;
+		}
+		if (isValidWord(word)) {
+			validWords.add(word);
+		}
+		marked[rows][cols] = true;
+		for (int i = rows - 1; i <= rows + 1; i++) {
+			for (int j = cols - 1; j <= cols + 1; j++) {
+				if (isValidRowColumn(i, j, board) && !marked[i][j]) {
+					String sequence = appendCharacter(word, board.getLetter(i, j));
+					dfs(board, marked, i, j, sequence);
+				}
+			}
+		}
+		marked[rows][cols] = false;
+	}
 
-   private String addLetter(String to, char letter) {
-      if (letter == Q_LETTER) {
-      	return to + QU_STRING;
-      } else {
-      	return to + letter;
-      }
-   }
-   
-    private boolean isValidWord(String currentWord) {
-      if (currentWord == null) return false;
-      if (dictionaryTrie.contains(currentWord) && currentWord.length() > 2) return true;
-      else return false;
-   }
+	private boolean isValidRowColumn(final int row, final int col, final BoggleBoard board) {
+		return (row >= 0 && col >= 0 && row < board.rows() && col < board.cols());
+	}
 
-
-	// Returns the score of the given word if it is in the dictionary, zero otherwise.
-	// (You can assume the word contains only the uppercase letters A through Z.)
-	public int scoreOf(String word) {
+	// Returns the score of the given word
+	// if it is in the dictionary, zero otherwise.
+	// (You can assume the word contains
+	// only the uppercase letters A through Z.)
+	public int scoreOf(final String word) {
+		if (word == null) return 0;
 		if (dictionaryTrie.contains(word)) {
 			return dictionaryTrie.get(word);
-		} return 0;
+		}
+		return 0;
 	}
 }
+
+
--- ADS-2-assignments/m16/boggle/TrieST.java
+++ ADS-2-assignments/m16/boggle/TrieST.java
@@ -1,5 +1,5 @@
 public class TrieST<Value> {
-    private static final int R = 256;        // extended ASCII
+    private static final int R = 26;        // extended ASCII
 
 
     private Node root;      // root of trie
@@ -48,7 +48,7 @@ public class TrieST<Value> {
         if (x == null) return null;
         if (d == key.length()) return x;
         char c = key.charAt(d);
-        return get(x.next[c], key, d+1);
+        return get(x.next[c - 65], key, d+1);
     }
 
     /**
@@ -68,12 +68,14 @@ public class TrieST<Value> {
     private Node put(Node x, String key, Value val, int d) {
         if (x == null) x = new Node();
         if (d == key.length()) {
-            if (x.val == null) n++;
+            if (x.val == null) {
+                n++;
+            }
             x.val = val;
             return x;
         }
         char c = key.charAt(d);
-        x.next[c] = put(x.next[c], key, val, d+1);
+        x.next[c - 65] = put(x.next[c - 65], key, val, d+1);
         return x;
     }
 
@@ -216,4 +218,10 @@ public class TrieST<Value> {
                 return x;
         return null;
     }
+    
+    public boolean hasPrefix(String query) {
+        Node x = get(root, query, 0);
+        return x != null;
+    }
+
 }
